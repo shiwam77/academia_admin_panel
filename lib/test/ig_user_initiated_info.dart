ea47d1eef93a8644f49f6bf12a7bf5ed0dabaf3d
shiwam77 <shiwamkarn77@gmail.com>
2021-03-31 14:31:59 +0545

    test for spockfe

--- lib/test/ig_user_initiated_info.dart
+++ lib/test/ig_user_initiated_info.dart
@@ -1,14 +1,6 @@
 
 import 'package:json_annotation/json_annotation.dart';
-
-import 'ig_business_category.dart';
-import 'ig_creator_interests.dart';
-
-
-
 part 'ig_user_initiated_info.g.dart';
-
-
 @JsonSerializable()
 class IgUserInitiatedInfo extends ApiSchema {
   final IgUser data;
@@ -25,23 +17,63 @@ class IgUserInitiatedInfo extends ApiSchema {
 
 @JsonSerializable()
 class IgUser {
-   final  String uuid;
-   final  String user_name;
-   final String email;
-   final String contact_no;
-   final String profile_type;
-   final bool is_active;
-   final bool is_deleted;
-   final IgCreatorDetails creator_details;
-   final IgBusinessDetails business_details;
-   final DateTime created_at;
-   final DateTime updated_at;
-   IgUser({ this.uuid, this.created_at, this. updated_at, this.business_details, this.contact_no,
-     this.creator_details, this.email, this.is_active, this.is_deleted, this.profile_type, this.user_name});
+  String uuid;
+  String user_name;
+  String email;
+  String contact_no;
+  String profile_type;
+  bool is_active;
+  bool is_deleted;
+  DateTime created_at;
+  DateTime updated_at;
+  IgCreatorDetails creator_details;
+  IgBusinessDetails business_details;
 
-   factory IgUser.fromJson(Map<String, dynamic> json) => _$IgUserFromJson(json);
+  IgUser({ this.user_name, this.profile_type, this.is_deleted, this.is_active, this.email, this.creator_details
+    ,this.contact_no, this.business_details, this.updated_at, this.created_at, this.uuid});
 
-   Map<String, dynamic> toJson() => _$IgUserToJson(this);
+  factory IgUser.fromJson(Map<String, dynamic> json) => _$IgUserFromJson(json);
+
+  Map<String, dynamic> toJson() => _$IgUserToJson(this);
+
+}
+
+@JsonSerializable()
+class IgBusinessDetails{
+  final String  id;
+  final List<String> gender;
+  final List<String> age_groups;
+  final String category;
+  final DateTime created_at;
+  final DateTime updated_at;
+
+  IgBusinessDetails( {this.id, this.age_groups, this.category, this.created_at, this.gender, this.updated_at});
+
+  factory IgBusinessDetails.fromJson(Map<String, dynamic> json) => _$IgBusinessDetailsFromJson(json);
+
+  Map<String, dynamic> toJson() => _$IgBusinessDetailsToJson(this);
+
+}
+@JsonSerializable()
+class IgCreatorDetails{
+  final String id ;
+  final List<String> interests ;
+  final String  pricing_currency ;
+  final double story_min;
+  final double story_max;
+  final double post_min;
+  final double post_max;
+  final double vid_min;
+  final double vid_max;
+  final DateTime created_at;
+  final DateTime updated_at;
+
+  IgCreatorDetails({this.id, this.updated_at, this.created_at ,this.interests, this.post_max ,
+    this.post_min, this.pricing_currency, this.story_max, this.story_min, this.vid_max, this.vid_min,});
+
+  factory IgCreatorDetails.fromJson(Map<String, dynamic> json) => _$IgCreatorDetailsFromJson(json);
+
+  Map<String, dynamic> toJson() => _$IgCreatorDetailsToJson(this);
 }
 
 
@@ -53,3 +85,24 @@ class ApiSchema {
 
   ApiSchema(this.status, this.msg, this.error_code, [this.error_data]);
 }
+
+// "data" = {
+// "user_name": "John",
+// "email" : "john@gmail.com",
+// "contact_no" : "+91 1234567899",
+// "profile_type" : "Creator",
+// "creator_details":{
+// "interests" : ["gaming", "fitness", "food"],
+// "pricing_currency" : "rupee",
+// "story_min" : 500,
+// "story_max" : 5000,
+// "post_min" : 750,
+// "post_max" : 7500,
+// "vid_min" : 1000,
+// "vid_max" : 10000,},
+// "business_details":{
+// "gender" : ["Male", "Female"],
+// "age_groups" : ["17-19", "24-30"],
+// "category" : "FoodandDrink"
+// }
+// }
